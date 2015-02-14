1d59b84938133d06bdf6b69f61b86337d68e3f9c
Jason Chung <jason.wk.chung@gmail.com>
2015-02-13 20:35:18 -0500

    add FNAME and LNAME, fix validation

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-var vid = document.getElementById("bgvid");
-var pauseButton = document.querySelector("#description button");
+// Pause Button Javascript
+// var vid = document.getElementById("bgvid");
+// var pauseButton = document.querySelector("#description button");
 
-function vidFade() {
-	vid.classList.add("stopfade");
-}
+// function vidFade() {
+// 	vid.classList.add("stopfade");
+// }
 
-vid.addEventListener('ended', function() {
-// only functional if "loop" is removed 
-vid.pause();
-// to capture IE10
-vidFade();
-}); 
+// vid.addEventListener('ended', function() {
+// 	// only functional if "loop" is removed 
+// 	vid.pause();
+// 	// to capture IE10
+// 	vidFade();
+// }); 
 
-pauseButton.addEventListener("click", function() {
-	vid.classList.toggle("stopfade");
-	if (vid.paused) {
-		vid.play();
-		pauseButton.innerHTML = "Pause";
-	} else {
-		vid.pause();
-		pauseButton.innerHTML = "Paused";
-	}
-})
+// pauseButton.addEventListener("click", function() {
+// 	vid.classList.toggle("stopfade");
+// 	if (vid.paused) {
+// 		vid.play();
+// 		pauseButton.innerHTML = "Pause";
+// 	} else {
+// 		vid.pause();
+// 		pauseButton.innerHTML = "Paused";
+// 	}
+// })
